e599ccc619055f3fd146d417a4876ab0f143b762
Baek Kwang Soo <spirytus43@naver.com>
2024-02-15 15:10:08 +0900

    Init

---
+++ public/test.php
@@ -0,0 +1,15 @@
+<?php
+
+$return = Array(
+    'member' => Array(
+        'name' => 'kwangsoo'
+    )
+    ,Array(
+        'name' => 'hanbyul'
+    )
+    ,Array(
+        'name' => 'sanghyuk'
+    )
+);
+
+echo json_encode($return);
